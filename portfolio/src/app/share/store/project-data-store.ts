79c11a7829e0c4c3908bbff6072dba779706a3d8
filippoerbisti <filippo.erbisti@gmail.com>
2022-01-05 15:36:03 +0100

    reverse order projects

--- portfolio/src/app/share/store/project-data-store.ts
+++ portfolio/src/app/share/store/project-data-store.ts
@@ -1,14 +1,6 @@
 import { Projects } from 'src/app/share/model/project.model';
 
 export var projects: Projects [] = [
-    { 
-        title: 'My Portfolio',
-        img: 'Complete and downloadable portfolio online. Powered and designed by me. ',
-        year: 2022,
-        description: 'This is my Portfolio Online',
-        url: 'filippoerbisti.netlify.app',
-        repo: 'github.com/filippoerbisti/my-portfolio'
-     },
      { 
         title: 'Ronda della Carità',
         img: '',
@@ -16,5 +8,13 @@ export var projects: Projects [] = [
         description: 'Digitization of the homeless data, order and card collection progress. Creation from zero to complete application, downloadable on any device.',
         url: 'ronda-della-carita.netlify.app',
         repo: 'github.com/filippoerbisti/ronda-della-carita'
-     }
+     },
+     { 
+      title: 'My Portfolio',
+      img: 'Complete and downloadable portfolio online. Powered and designed by me. ',
+      year: 2022,
+      description: 'This is my Portfolio Online',
+      url: 'filippoerbisti.netlify.app',
+      repo: 'github.com/filippoerbisti/my-portfolio'
+   }
 ];
