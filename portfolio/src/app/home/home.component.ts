882ee92f31dc4544df7612f437702c8ffea29cbe
filippoerbisti <filippo.erbisti@gmail.com>
2022-01-06 00:22:00 +0100

    create var for each skill

--- portfolio/src/app/home/home.component.ts
+++ portfolio/src/app/home/home.component.ts
@@ -12,6 +12,17 @@ export class HomeComponent implements OnInit {
   color: ThemePalette = 'primary';
   mode: ProgressBarMode = 'determinate';
 
+  angularSkill = 85;
+  vueSkill = 70;
+  cssSkill = 70;
+  typescriptSkill = 75;
+
+  sqlSkill = 80;
+  laravelSkill = 80;
+  phpSkill = 75;
+  csharpSkill = 50;
+  
+
   constructor() { }
 
   ngOnInit(): void {
