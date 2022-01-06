28e0d5df79eba814a7e00875ed39413be20ea122
filippoerbisti <filippo.erbisti@gmail.com>
2022-01-06 20:51:24 +0100

    button top scroll

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'portfolio';
+
+  @Output() scrollToTop = new EventEmitter<void>();
+
+  constructor() { }
+
+  onScrollToTop(): void {
+    this.scrollToTop.emit();
+  }
+
 }
